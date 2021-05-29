d4cb6316be44a8c86f69d60653c6fd13ef28014b
Santhosh <47562746+ajsanthosh14@users.noreply.github.com>
2021-05-29 12:21:26 +0530

    Update selfbal.py

--- selfbal.py
+++ selfbal.py
@@ -1,6 +1,7 @@
 import smbus
 import time
 import math
+import Adafruit_BBIO.PWM as PWM
 
 
 PWR_MGMT_1   = 0x6B
@@ -26,6 +27,43 @@ K  = 1.9*1.12
 K0 =  0.98
 K1 =  0.02
 
+
+#initializing motors
+LF="P8_13"
+LB="P8_19"
+RF="P9_21"
+RB="P9_22"
+
+PWM.start(LF,0,1000)
+PWM.start(LB,0,1000)
+PWM.start(RF,0,1000)
+PWM.start(RB,0,1000)
+
+def stop_motors():
+	PWM.set_duty_cycle(LF,0)
+	PWM.set_duty_cycle(LB,0)
+	PWM.set_duty_cycle(RF,0)
+	PWM.set_duty_cycle(RB,0)
+
+def motors(speed,left_offset,right_offset):
+	left_speed = speed + left_offset
+	right_speed = speed + right_offset
+	
+	#left motor
+	if left_speed<0:
+		PWM.set_duty_cycle(LF,-left_speed)
+		PWM.set_duty_cycle(LB,0)
+	elif left_speed>0:
+		PWM.set_duty_cycle(LB,left_speed)                
+		PWM.set_duty_cycle(LF,0)        
+
+	#right motor
+	if right_speed<0:
+		PWM.set_duty_cycle(RF,-right_speed)
+		PWM.set_duty_cycle(RB,0)
+	elif right_speed>0:
+		PWM.set_duty_cycle(RB,right_speed)
+		PWM.set_duty_cycle(RF,0)
 
 def MPU_Init():
 	#write to sample rate register
@@ -192,6 +230,12 @@ if __name__ == "__main__":
 	  
 
 	  	stop_motors();
+
+PWM.stop(LF)
+PWM.stop(LB)
+PWM.stop(RF)
+PWM.stop(RB)
+PWM.cleanup()
 	  
     		
     	
